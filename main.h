4ecd1d45e12d9f048b2fd547ee47b90cd4744a3f
collins ibeh <ibehcollins@gmail.com>
2023-07-25 17:08:23 -0700

    printf

--- main.h
+++ main.h
@@ -25,13 +25,7 @@ int _printf(const char *format, ...);
 int main(void);
 int ce_main(void);
 int _print_main(void);
-<<<<<<< HEAD
-=======
-int _format_specifier(const char *format, ...);
-<<<<<<< HEAD
->>>>>>> 6bcdc8fb364a9e57d42e0fac846c3354a3145bbf
-=======
-int printInteger(int num);
->>>>>>> 19515e4d7237fd32a15601a0358adb6bb7007c13
+void print_binary(unsigned int n);
+
 
 #endif
