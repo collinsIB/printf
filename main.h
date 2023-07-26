28ce40f4fe3ed597283ec51e7f13d9ae8f78cc99
collins ibeh <ibehcollins@gmail.com>
2023-07-25 19:34:47 -0700

    update

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * function prototype
+ */
+
+int _putchar(char o);
+int _print_str(char *s);
+int _printf(const char *format, ...);
+int _format(char form_specifier, va_list arg_ptr);
+int _print_my_digit(int value, int base);
+void print_char(va_list args, int *count);
+void printf_strings(va_list args, int *count);
+void print_percent(int *count);
+int _printf(const char *format, ...);
+int main(void);
+int ce_main(void);
+int _print_main(void);
+void print_binary(unsigned int n);
+
+#endif /* MAIN_H */
